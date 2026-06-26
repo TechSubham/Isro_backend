e249bb0d27d6bb6dd1734c70e51da9a7b18ec0dd
TechSubham <subhamchauhan523@gmail.com>
2026-06-26 22:18:43 +0530

    cors fix

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = FastAPI(title="Solar Sync Model API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://isro-frontend-p8oc.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
